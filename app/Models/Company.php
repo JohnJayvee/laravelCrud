a71c16d739fa8e1ca8ad89a0abb809fa96f7a86e
JohnJayvee <johnjayveesiuagan@gmail.com>
2023-08-26 15:32:28 +0800

    added model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $table = 'companies';
+    protected $fillable = [
+        'name',
+        'email',
+        'address'
+    ];
 }
