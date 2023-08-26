5ae42a2fd6a1ba6b57f65c57edda5027343c17df
JohnJayvee <johnjayveesiuagan@gmail.com>
2023-08-26 18:51:04 +0800

    added routes namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,8 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
+    protected $namespace = 'App\\Http\\Controllers';
+
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
@@ -34,6 +36,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
